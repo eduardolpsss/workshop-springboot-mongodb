35e466bf364f6cdac5c877690f7154fdb2e20512
eduardolpsss <eduardo.pontes2801@gmail.com>
2024-05-21 11:06:54 -0300

    Entity Post e projeção de dados

--- src/main/java/com/wsexample/course/config/Instantiation.java
+++ src/main/java/com/wsexample/course/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.wsexample.course.config;
 
+import com.wsexample.course.domain.Post;
 import com.wsexample.course.domain.User;
+import com.wsexample.course.dto.AuthorDTO;
+import com.wsexample.course.repository.PostRepository;
 import com.wsexample.course.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,15 +19,30 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userReposiroty;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userReposiroty.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userReposiroty.save(maria);
     }
 }
